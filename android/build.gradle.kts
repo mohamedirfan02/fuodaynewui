84a11f488affab6ae19bec23165f5889fd9e2b68
Mohamed-Irfan <mohamedirfanmohamed08@gmail.com>
2025-10-30 16:28:34 +0530

    16kb page memorry updated

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,9 +4,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:8.12.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.gms:google-services:4.4.2")
     }
 }
 
